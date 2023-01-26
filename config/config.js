66a9d4a964f35603a29f2a8c090454d6c68b189f
Kedar Gogi <49951070+kedargogi@users.noreply.github.com>
2023-01-26 19:47:45 +0530

    Delete config.js
    update config file

--- config/config.js
+++
@@ -1,7 +0,0 @@
-const config={
-    PORT:'5000',
-    JWT_SECRET:'zerozilla',
-    DB_URL:'mongodb+srv://kedargogi:kedar%4091@cluster0.zggp1ae.mongodb.net/?retryWrites=true&w=majority'
-
-}
-module.exports=config
